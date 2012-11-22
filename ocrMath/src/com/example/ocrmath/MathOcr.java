c9bb7e167c038b4590fb89afadab2a18b095f7ac
Alistair Dobke <adobke@gmail.com>
2012-11-21 22:15:37 -0800

    Fix cropVertically

--- ocrMath/src/com/example/ocrmath/MathOcr.java
+++ ocrMath/src/com/example/ocrmath/MathOcr.java
@@ -3,7 +3,10 @@ package com.example.ocrmath;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MathOcr extends Activity {
 
@@ -11,7 +14,19 @@ public class MathOcr extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_ocr);
+        ImageView start = (ImageView) findViewById(R.id.baseImage);
+        ImageView gray = (ImageView) findViewById(R.id.grayedImage);
+        ImageView crop = (ImageView) findViewById(R.id.croppedImage);
+
         
+        Bitmap testImg = BitmapFactory.decodeResource(getResources(), R.drawable.test1p1);
+        start.setImageBitmap(testImg);
+        
+        Bitmap grayScale = toGrayscale(testImg);
+        gray.setImageBitmap(grayScale);
+
+        Bitmap cropped = cropVertically(grayScale);
+        crop.setImageBitmap(cropped);
     }
 
     @Override
@@ -31,6 +46,8 @@ public class MathOcr extends Activity {
     			avg += (input.getPixel(j, i) >> 8) & 0xFF;
     			avg += (input.getPixel(j, i) & 0xFF);
     			avg /= 3;
+    			avg &= 0xFF;
+    			avg = (avg << 16) | (avg << 8) | avg;
     			grayScaled.setPixel(j, i, avg);
     		}
     	}
@@ -40,14 +57,15 @@ public class MathOcr extends Activity {
     public Bitmap cropVertically(Bitmap input) {
     	int topIndex = -1;
     	int botIndex = -1;
-    	int THRESH = 100;
+    	int THRESH = 125;
     	
     	// Find top and bottom indices
+    	
     	for(int i = 0; i < input.getHeight(); ++i) {
     		boolean found = false;
     		// Check each row
     		for(int j = 0; j < input.getWidth(); ++j) {
-    			if(input.getPixel(j, i) > THRESH)
+    			if( (input.getPixel(j, i) & 0xFF ) < THRESH)
     				found = true;
     		}
     		if(found) {
@@ -56,11 +74,11 @@ public class MathOcr extends Activity {
     		}
     	}
     	
-    	for(int i = input.getHeight(); i > 0; ++i) {
+    	for(int i = input.getHeight()-1; i >= 0; --i) {
     		boolean found = false;
     		// Check each row
     		for(int j = 0; j < input.getWidth(); ++j) {
-    			if(input.getPixel(j, i) > THRESH)
+    			if( (input.getPixel(j, i) & 0xFF) < THRESH)
     				found = true;
     		}
     		if(found) {
@@ -69,11 +87,17 @@ public class MathOcr extends Activity {
     		}
     	}
     	
+
+    	Log.v("ocrdebug","top: " + topIndex);
+    	Log.v("ocrdebug","bot: " + botIndex);
+    	
     	Bitmap cropped = Bitmap.createBitmap(input.getWidth(),Math.abs(botIndex - topIndex),input.getConfig());
     	
+    	
     	for(int i = 0; i < Math.abs(botIndex - topIndex); ++i) {
+        	Log.v("ocrdebug","i: " + i);
     		for(int j = 0; j < input.getWidth(); ++j) {
-    			cropped.setPixel(j, i, input.getPixel(topIndex+i, j));
+    			cropped.setPixel(j, i, input.getPixel(j,topIndex+i));
     		}
     	}
     	
